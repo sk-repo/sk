6ea94448bf16945919330bc3ebe0c6cad422733b
jribacruz <jribacruz@gmail.com>
2017-01-30 20:56:49 -0300

    first commit

---
+++ sk/src/main/java/br/sk/SkApplication.java
@@ -0,0 +1,12 @@
+package br.sk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkApplication.class, args);
+	}
+}
