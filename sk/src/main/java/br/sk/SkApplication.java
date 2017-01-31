65dc11d30d514d77a7493861986a1e476f441c1c
jribacruz <jribacruz@gmail.com>
2017-01-31 09:58:41 -0300

    ajuste no core

--- sk/src/main/java/br/sk/SkApplication.java
+++ sk/src/main/java/br/sk/SkApplication.java
@@ -7,13 +7,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import br.sk.internal.action.ActionDispatcher;
-import br.sk.internal.action.ActionLoader;
 
 @SpringBootApplication
 public class SkApplication implements CommandLineRunner {
-
-	@Autowired
-	private ActionLoader actionLoader;
 
 	@Autowired
 	private ActionDispatcher actionDispatcher;
