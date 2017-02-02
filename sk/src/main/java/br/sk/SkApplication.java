143b5313d93bf88ce059680dec03cb02fd49c9d8
jribacruz <jribacruz@gmail.com>
2017-02-02 20:37:39 -0300

    first commit

--- sk/src/main/java/br/sk/SkApplication.java
+++ sk/src/main/java/br/sk/SkApplication.java
@@ -1,8 +1,5 @@
 package br.sk;
 
-import org.fusesource.jansi.Ansi;
-import org.fusesource.jansi.Ansi.Color;
-import org.fusesource.jansi.AnsiConsole;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,13 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SkApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
-		AnsiConsole.systemInstall();
 		SpringApplication.run(SkApplication.class, args);
-		AnsiConsole.systemUninstall();
 	}
 
 	@Override
 	public void run(String... arg0) throws Exception {
-		Ansi.ansi().fg(Color.CYAN).a("Funcinou!");
 	}
 }
